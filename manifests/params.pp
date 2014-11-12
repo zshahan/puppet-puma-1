5383824261b8a59047f4bd504100bfcc7dc08eca
Mike Walsh <mwalsh@deversus.com>
2014-11-12 11:43:55 -0800

    Change default pid path

--- manifests/params.pp
+++ manifests/params.pp
@@ -15,11 +15,11 @@ class puma::params {
 	case $::osfamily {
 		'Debian': {
 			$app_root_spf			= '/var/www/%s'
-			$puma_pid_path_spf		= "/var/run/%s/puma.pid"
-			$puma_socket_path_spf	= "/var/run/%s/puma.socket"
-			$puma_config_path_spf	= "/etc/%s/puma.rb"	
-			$puma_stdout_log_path_spf = "/var/log/%s.puma.stdout.log"	
-			$puma_stderr_log_path_spf = "/var/log/%s.puma.stderr.log"	
+			$puma_pid_path_spf		= "/var/%s/pids/puma.pid"
+			$puma_socket_path_spf	= "/var/%s/pids/puma.socket"
+			$puma_config_path_spf	= "/etc/%s/puma.rb"
+			$puma_stdout_log_path_spf = "/var/log/%s.puma.stdout.log"
+			$puma_stderr_log_path_spf = "/var/log/%s.puma.stderr.log"
 			$init_script_spf		= "/etc/init.d/%s"
 			unless $www_user {
 				$www_user 			= 'www-data' # generic debian web-server user
@@ -36,6 +36,6 @@ class puma::params {
 		default: {
 			fail("${::osfamily} is not supported - now would be a great time to look into using module_data")
 		}
-		
+
 	}
 }
