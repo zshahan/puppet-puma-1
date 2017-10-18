95188b5aa82170020b5a61df4fc5cc5660e9f215
Trey Stafford <trey.stafford@nsidc.org>
2017-10-18 13:07:01 -0600

    Linting

--- manifests/nginxconfig.pp
+++ manifests/nginxconfig.pp
@@ -1,75 +1,74 @@
+# puma::nginxconfig
 define puma::nginxconfig(
-	$app_name 		= $title,
-	$server_name    = [$title],
-	$public_root	= join([sprintf($puma::app_root_spf, $title), "public"], '/'),
+  $app_name     = $title,
+  $server_name    = [$title],
+  $public_root  = join([sprintf($puma::app_root_spf, $title), 'public'], '/'),
 ) {
-	$puma_socket_path = sprintf($puma::puma_socket_path_spf, $app_name)
+  $puma_socket_path = sprintf($puma::puma_socket_path_spf, $app_name)
 
-	$upstream_name = "${app_name}_socket"
-	$vhost_name = $app_name
+  $upstream_name = "${app_name}_socket"
+  $vhost_name = $app_name
 
-	nginx::resource::upstream {$upstream_name:
-		ensure 	=> present,
-		members	=> ["unix://$puma_socket_path"],
-		upstream_fail_timeout => '10s',
-	}
+  nginx::resource::upstream {$upstream_name:
+    ensure                => present,
+    members               => ["unix://${puma_socket_path}"],
+    upstream_fail_timeout => '10s',
+  }
 
 
-	nginx::resource::vhost { $vhost_name:
-		ensure 			=> present,
-		server_name 	=> $server_name,
-		www_root		=> $public_root,
-		use_default_location => false,
-		index_files 	=> [],
-	}
+  nginx::resource::vhost { $vhost_name:
+    ensure               => present,
+    server_name          => $server_name,
+    www_root             => $public_root,
+    use_default_location => false,
+    index_files          => [],
+  }
 
-	nginx::resource::location { "${vhost_name}:assets":
-		location 			  						 => '~ ^/(assets)/',
-		ensure               				 =>  present,
-		vhost                				 =>  $vhost_name,
-		priority             				 =>  499,
-		location_custom_cfg  				 =>  {
-			'access_log'   =>  'off',
-			'expires'      =>  'max',
-			'add_header'   =>  'Cache-Control "public"',
-			'add_header'   =>  'Etag ""',
-			'gzip_static'  =>  'on',
-		},
-		location_custom_cfg_append	=> {
-			'if' => '($request_filename ~* ^.*?\.(eot)|(ttf)|(woff)|(svg)|(otf)$){
-				add_header Access-Control-Allow-Origin *;
-			}
-			'
-		},
-	}
+  nginx::resource::location { "${vhost_name}:assets":
+    ensure                     => present,
+    location                   => '~ ^/(assets)/',
+    vhost                      => $vhost_name,
+    priority                   => 499,
+    location_custom_cfg        => {
+      'access_log'  => 'off',
+      'expires'     => 'max',
+      'add_header'  => 'Cache-Control "public"',
+      'add_header'  => 'Etag ""',
+      'gzip_static' => 'on',
+    },
+    location_custom_cfg_append => {
+      'if' => '($request_filename ~* ^.*?\.(eot)|(ttf)|(woff)|(svg)|(otf)$){
+        add_header Access-Control-Allow-Origin *;
+      }
+      '
+    },
+  }
 
-	nginx::resource::location { "${vhost_name}:root":
-		location 			=> '/',
-		ensure 				=> present,
-		vhost 				=> $vhost_name,
-		priority			=> 500,
-		location_custom_cfg => {
-			try_files => ['$uri @rails']
-		}
+  nginx::resource::location { "${vhost_name}:root":
+    ensure              => present,
+    location            => '/',
+    vhost               => $vhost_name,
+    priority            => 500,
+    location_custom_cfg => {
+      try_files => ['$uri @rails']
+    }
+  }
 
-	}
-
-	nginx::resource::location { "${vhost_name}:rails":
-		location 			  => '@rails',
-		ensure                =>  present,
-		priority              =>  501,
-		vhost                 =>  $vhost_name,
-		proxy                 =>  "http://${upstream_name}",
-		proxy_read_timeout    =>  '90',
-		location_cfg_append   =>  {
-			'proxy_http_version'  =>  '1.1',
-			'proxy_set_header'    =>  [
-				'Host $host',
-				'X-Forwarded-For $proxy_add_x_forwarded_for',
+  nginx::resource::location { "${vhost_name}:rails":
+    ensure              => present,
+    location            => '@rails',
+    priority            => 501,
+    vhost               => $vhost_name,
+    proxy               => "http://${upstream_name}",
+    proxy_read_timeout  => '90',
+    location_cfg_append => {
+      'proxy_http_version' => '1.1',
+      'proxy_set_header'   => [
+        'Host $host',
+        'X-Forwarded-For $proxy_add_x_forwarded_for',
         # Thought this was needed for SSL but it is causing an infinite loop
-				# 'X-Forwarded-Proto $scheme'
-			],
-		},
-	}
-
+        # 'X-Forwarded-Proto $scheme'
+      ],
+    },
+  }
 }
--- manifests/rvm.pp
+++ manifests/rvm.pp
@@ -1,19 +1,19 @@
+# puma::rvm
 define puma::rvm(
-	$app_name = $title,
-	$rvm_ruby,
+  $rvm_ruby,
+  $app_name = $title,
 ) {
-	ensure_resource('class', 'rvm')
-	ensure_resource('rvm::system_user', $puma::puma_user)
-	ensure_resource('rvm_system_ruby', $rvm_ruby, {'ensure'=>'present'})
-	
-	Rvm::System_user[$puma::puma_user]
-	-> Rvm_system_ruby[$rvm_ruby]
-	-> rvm_gemset {"$rvm_ruby@$app_name":
-		require => Rvm_system_ruby[$rvm_ruby],
-		ensure  => present,
-	}
-	-> rvm_gem {"$rvm_ruby@$app_name/bundler":
-		ensure  => present,
-	}
-
+  ensure_resource('class', 'rvm')
+  ensure_resource('rvm::system_user', $puma::puma_user)
+  ensure_resource('rvm_system_ruby', $rvm_ruby, {'ensure'=>'present'})
+  
+  Rvm::System_user[$puma::puma_user]
+  -> Rvm_system_ruby[$rvm_ruby]
+  -> rvm_gemset {"${rvm_ruby}@${app_name}":
+    ensure  => present,
+    require => Rvm_system_ruby[$rvm_ruby],
+  }
+  -> rvm_gem {"${rvm_ruby}@${app_name}/bundler":
+    ensure  => present,
+  }
 }
