7ed723a81684ee7d05083a81a607d22d259a54c5
Zack Shahan <zack.shahan@funeraldirectorslife.com>
2019-06-10 15:19:50 -0500

    test

--- manifests/app.pp
+++ manifests/app.pp
@@ -165,7 +165,7 @@ define puma::app (
         mode    => '0755',
         require => File["${app_root}/shared/bin"]
       }
-      file { ["${app_root}/shared/bin","${app_root}/shared","${app_root}/shared/tmp"]:
+      file { ["${app_root}/shared/bin","${app_root}/shared"]:
         ensure => directory,
         owner  => $puma_user,
         group  => $puma_user,
